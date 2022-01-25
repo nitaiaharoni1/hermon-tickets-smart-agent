5a42dcf932340609cf55a853527c4f1cd9e3682d
nitaiaharoni <nitaiaharoni1@gmail.com>
2022-01-25 13:55:32 +0200

    asd

--- index.js
+++ index.js
@@ -1,22 +1,21 @@
 const axios = require("axios");
 const nodemailer = require("nodemailer");
+const config = require("./config");
 
 let interval;
-const RECIPIENTS = ['email@gmail.com']
-const INTERVAL_TIME = 1000 * 60 * 3;
 
 const transporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
-        user: 'EMAIL',
-        pass: 'PASSWORD'
+    user: config.MY_EMAIL,
+    pass: config.MY_EMAIL_PASSWORD
   }
 });
 
 let sendMail = (date) => {
   const mailOptions = {
-    from: 'MY_EMAIL@gmail.com',
-    to: RECIPIENTS.join(','),
+    from: config.MY_EMAIL,
+    to: config.RECIPIENTS.join(','),
     subject: 'Hermon Buy Tickets!',
     text: `The date ${date} is available. buy tickets at: \n https://hermon.pres.global/vouchers`
   };
@@ -31,9 +30,9 @@ let sendMail = (date) => {
   });
 }
 
-let getIsDateAvailable = (res, date) => {
+let getIsDateAvailable = (res, minDate) => {
   try {
-    const isDateAvailable = res.some(item => (+item.Date.split('-')[2]) >= date);
+    const isDateAvailable = res.some(item => (+item.Date.split('-')[2]) >= minDate);
     return isDateAvailable;
   } catch (e) {
     console.log(e);
@@ -43,7 +42,7 @@ let getIsDateAvailable = (res, date) => {
 
 let getDates = async () => {
   try {
-    const res = await axios("https://hermon.pres.global/api/system-vouchers/byDate?recaptchaToken=TOKEN")
+    const res = await axios(`https://hermon.pres.global/api/system-vouchers/byDate?recaptchaToken=${config.HERMON_RECAPTCHA_TOKEN}`)
     return res.data.Dates;
   } catch (e) {
     console.log(e);
@@ -51,24 +50,28 @@ let getDates = async () => {
 
 }
 
-const main = async (date) => {
+const main = async (minDate) => {
   console.log("start", new Date());
   const res = await getDates();
-  const isDateAvailable = getIsDateAvailable(res, date);
+  const isDateAvailable = getIsDateAvailable(res, minDate);
   console.log("isDateAvailable:", isDateAvailable);
   if (isDateAvailable) {
-    sendMail(date)
+    sendMail(minDate)
   }
 }
 
-const run = (minDate) => {
-  console.log("Interval time: ", INTERVAL_TIME)
+const run = () => {
+  const minDate = +process.argv.slice(2)[0]
+  if (!minDate || isNaN(minDate) || minDate < 1 || minDate > 31) {
+    throw new Error("Please provide a min day number 1-31")
+  }
+  console.log("Interval time: ", config.INTERVAL_TIME)
   console.log("Min date: ", minDate)
-  console.log("Recipients: ", RECIPIENTS.join(', '))
+  console.log("Recipients: ", config.RECIPIENTS.join(', '))
   main(minDate);
   interval = setInterval(() => {
     main(minDate);
-  }, INTERVAL_TIME);
+  }, config.INTERVAL_TIME);
 }
 
-run(28)
+run()
