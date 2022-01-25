b55bcc3426aeb2187d58f5b6d30148c59773497e
nitaiaharoni <nitaiaharoni1@gmail.com>
2022-01-25 13:29:49 +0200

    asd

--- index.js
+++ index.js
@@ -2,70 +2,73 @@ const axios = require("axios");
 const nodemailer = require("nodemailer");
 
 let interval;
+const RECIPIENTS = ['email@gmail.com']
+const INTERVAL_TIME = 1000 * 60 * 3;
+
 const transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
+  service: 'gmail',
+  auth: {
         user: 'EMAIL',
         pass: 'PASSWORD'
-    }
+  }
 });
 
 let sendMail = (date) => {
-    const arrayUsersMail = ['mail@gmail.com'].join(',')
-
-    const mailOptions = {
-        from: 'nitaiaharoni1@gmail.com',
-        to: arrayUsersMail,
-        subject: 'Hermon Buy Tickets!',
-        text: `The date ${date} is available. buy tickets at: \n https://hermon.pres.global/vouchers`
-    };
-    transporter.sendMail(mailOptions, function (error, info) {
-        if (error) {
-            console.log(error);
-        } else {
-            console.log('Email sent: ' + info.response);
-            clearInterval(interval);
-            process.exit()
-        }
-    });
+  const mailOptions = {
+    from: 'MY_EMAIL@gmail.com',
+    to: RECIPIENTS.join(','),
+    subject: 'Hermon Buy Tickets!',
+    text: `The date ${date} is available. buy tickets at: \n https://hermon.pres.global/vouchers`
+  };
+  transporter.sendMail(mailOptions, function (error, info) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log('Email sent: ' + info.response);
+      clearInterval(interval);
+      process.exit()
+    }
+  });
 }
 
 let getIsDateAvailable = (res, date) => {
-    try {
-        const isDateAvailable = res.some(item => (+item.Date.split('-')[2]) >= date);
-        return isDateAvailable;
-    } catch (e) {
-        console.log(e);
-        return false;
-    }
+  try {
+    const isDateAvailable = res.some(item => (+item.Date.split('-')[2]) >= date);
+    return isDateAvailable;
+  } catch (e) {
+    console.log(e);
+    return false;
+  }
 }
 
 let getDates = async () => {
-    try {
-        const res = await axios("https://hermon.pres.global/api/system-vouchers/byDate?recaptchaToken=TOKEN")
-        return res.data.Dates;
-    } catch (e) {
-        console.log(e);
-    }
+  try {
+    const res = await axios("https://hermon.pres.global/api/system-vouchers/byDate?recaptchaToken=TOKEN")
+    return res.data.Dates;
+  } catch (e) {
+    console.log(e);
+  }
 
 }
 
 const main = async (date) => {
-    console.log("start", new Date());
-    const res = await getDates();
-    const isDateAvailable = getIsDateAvailable(res, date);
-    console.log("isDateAvailable:", isDateAvailable);
-    if (isDateAvailable) {
-        sendMail(date)
-    }
+  console.log("start", new Date());
+  const res = await getDates();
+  const isDateAvailable = getIsDateAvailable(res, date);
+  console.log("isDateAvailable:", isDateAvailable);
+  if (isDateAvailable) {
+    sendMail(date)
+  }
 }
 
 const run = (minDate) => {
-    console.log("Min date is: ", minDate)
+  console.log("Interval time: ", INTERVAL_TIME)
+  console.log("Min date: ", minDate)
+  console.log("Recipients: ", RECIPIENTS.join(', '))
+  main(minDate);
+  interval = setInterval(() => {
     main(minDate);
-    interval = setInterval(() => {
-        main(minDate);
-    }, 1000 * 60);
+  }, INTERVAL_TIME);
 }
 
 run(28)
