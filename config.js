926bcc2bb721caa5d43904ec5209d2b5099a3491
nitaiaharoni <nitaiaharoni1@gmail.com>
2022-01-25 13:57:26 +0200

    asd

---
+++ config.js
@@ -0,0 +1,9 @@
+const config = {
+  RECIPIENTS: ['<RECIPIENT@gmail.com>'],
+  MY_EMAIL: '<MY_EMAIL@gmail.com>',
+  MY_EMAIL_PASSWORD: '<MY_EMAIL_PASSWORD>',
+  INTERVAL_TIME: 1000 * 60 * 3,
+  HERMON_RECAPTCHA_TOKEN: '<HERMON_RECAPTCHA_TOKEN>'
+}
+
+module.exports = config;
